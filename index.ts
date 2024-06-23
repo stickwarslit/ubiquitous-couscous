8d9541f92334466952ed3aa0ec38833b2b9a0a57
Nora Del Rosario <stickwarslit@gmail.com>
2024-06-23 12:10:09 -0400

    ci: Add formatting

--- index.ts
+++ index.ts
@@ -1,36 +1,35 @@
-import { PrismaClient } from "@prisma/client"
-import { closeBrowser, withPage } from "./src/browser"
+import { PrismaClient } from '@prisma/client';
+import { closeBrowser, withPage } from './src/browser';
 
 const VOMA_CIRCLE_LISTS = [
-'https://ketto.xsrv.jp/html/mimiken/clist.cgi?tvm55'
-]
+  'https://ketto.xsrv.jp/html/mimiken/clist.cgi?tvm55',
+];
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
-    for (const url of VOMA_CIRCLE_LISTS) {
-        await withPage(async (page) => {
-            await page.goto(url, { waitUntil: 'networkidle0' })
-            await page.waitForSelector('center > table', { visible: true })
-            const html = await page.$eval('html', (el) => el.outerHTML)
+  for (const url of VOMA_CIRCLE_LISTS) {
+    await withPage(async (page) => {
+      await page.goto(url, { waitUntil: 'networkidle0' });
+      await page.waitForSelector('center > table', { visible: true });
+      const html = await page.$eval('html', (el) => el.outerHTML);
 
-            await prisma.htmlScrape.create({
-                data: {
-                    html,
-                    url
-                }
-            })
-
-        })
-    }
+      await prisma.htmlScrape.create({
+        data: {
+          html,
+          url,
+        },
+      });
+    });
+  }
 }
 
 try {
-    await main()
-    await prisma.$disconnect()
+  await main();
+  await prisma.$disconnect();
 } catch (e) {
-    console.error(e)
+  console.error(e);
 } finally {
-    await prisma.$disconnect()
-    await closeBrowser()
+  await prisma.$disconnect();
+  await closeBrowser();
 }
