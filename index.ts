7e6c0eea8cb2aa974fa39fc682a95571ae79406a
Nora Del Rosario <stickwarslit@gmail.com>
2024-06-29 14:48:21 -0400

    feat: Add vopara scrape

--- index.ts
+++ index.ts
@@ -2,9 +2,14 @@ import { closeBrowser, withPage } from './src/browser';
 import { createExport } from './src/export';
 import { prisma } from './src/prisma';
 import { scrapeVomaUrl } from './src/voma-scrape';
+import { scrapeVoparaUrl } from './src/vopara-scrape';
 
 const VOMA_CIRCLE_LISTS = [
   'https://ketto.xsrv.jp/html/mimiken/clist.cgi?tvm55',
+];
+
+const VOPARA_CIRCLE_LISTS = [
+  'https://ttc.ninja-web.net/vo-para/vo-para13_list.htm',
 ];
 
 async function main() {
@@ -19,8 +24,14 @@ async function main() {
   await Bun.write('output.xlsx', data);
 }
 
+async function main1() {
+  for (const url of VOPARA_CIRCLE_LISTS) {
+    await scrapeVoparaUrl(url);
+  }
+}
+
 try {
-  await main();
+  await main1();
   await prisma.$disconnect();
 } catch (e) {
   console.error(e);
---
+++ src/vopara-scrape.ts
@@ -0,0 +1,106 @@
+import { prisma } from './prisma';
+import { JSDOM } from 'jsdom';
+import { withPage } from './browser';
+import { EventSeries } from './event-series';
+import assert from 'node:assert';
+
+export async function scrapeVoparaUrl(url: string) {
+  const webPage = await getOrFetchVoparaPageHTML(url);
+
+  const dom = new JSDOM(webPage.html);
+  const document = dom.window.document;
+
+  const title =
+    document.querySelector('body > p:nth-of-type(1)')?.textContent ?? '';
+  const eventTag = title?.match(/ボーパラ(\d+)/)?.[0];
+
+  if (!title || !eventTag) {
+    console.log({ title, eventTag, url });
+    process.exit(1);
+  }
+
+  const event = await prisma.event.upsert({
+    where: { tag: eventTag },
+    create: {
+      tag: eventTag,
+      title,
+      webPage: { connect: { id: webPage.id } },
+      series: EventSeries.Vopara,
+    },
+    update: {},
+  });
+
+  const circleList = [
+    ...document.querySelectorAll('body > table > tbody > tr'),
+  ].filter(
+    ($tr) =>
+      !$tr.textContent?.includes('サークル名') &&
+      $tr.children.length > 1 &&
+      !$tr.textContent?.includes('AHSストア')
+  );
+
+  const circlesFormatted = circleList.map(($tr, i) => {
+    try {
+      const rowCells = $tr.querySelectorAll('td');
+      const name = rowCells[0].textContent;
+      const penname = rowCells[1].textContent;
+      const booth = rowCells[4].textContent;
+
+      assert(name);
+      assert(penname);
+      assert(booth);
+
+      const links = [...$tr.querySelectorAll('a')]
+        .map(($a) => $a.href)
+        .filter((href) => href.includes('http'));
+      return { name, penname, booth, links };
+    } catch (e) {
+      console.error(e);
+      console.log({ html: $tr.outerHTML, text: $tr.textContent, i });
+      process.exit(1);
+    }
+  });
+
+  for (const circle of circlesFormatted) {
+    const { name, penname, booth, links } = circle;
+
+    await prisma.circleEventInfo.upsert({
+      where: { eventCircle: { name, eventId: event.id } },
+      create: {
+        name,
+        penname,
+        booth,
+        event: { connect: { id: event.id } },
+        links: { create: links.map((url) => ({ url })) },
+      },
+      update: {},
+    });
+  }
+}
+
+async function getOrFetchVoparaPageHTML(url: string) {
+  const webPage = await prisma.webPage.findFirst({ where: { url } });
+
+  if (webPage) {
+    return webPage;
+  }
+
+  const html = await fetchVoparaPageHTML(url);
+  return await prisma.webPage.create({
+    data: {
+      html,
+      url,
+    },
+  });
+}
+
+async function fetchVoparaPageHTML(url: string) {
+  return await withPage(async (page) => {
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    await page.waitForSelector('body > table:nth-of-type(2)', {
+      visible: true,
+    });
+    const html = await page.$eval('html', (el) => el.outerHTML);
+    return html;
+  });
+}
